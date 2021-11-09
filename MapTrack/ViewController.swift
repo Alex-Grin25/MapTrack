ad62d741f85af036d8eca6a4182bd61800525f72
Alex-Grin25 <lona.ai@yahoo.com>
2021-11-08 23:27:20 -0500

    Add map and markers on location update

--- MapTrack/ViewController.swift
+++ MapTrack/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        let camera = GMSCameraPosition.camera(withLatitude: <#T##CLLocationDegrees#>, longitude: <#T##CLLocationDegrees#>, zoom: <#T##Float#>)
+        mapView = GMSMapView.map(withFrame: self.view.frame, camera: GMSCameraPosition())
+        self.view.addSubview(mapView)
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let coordinate = location.coordinate
+            self.mapView.camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 17)
+            let marker = GMSMarker(position: coordinate)
+            marker.map = self.mapView
+        }
+    }
+}
